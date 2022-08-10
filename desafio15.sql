9c1148ba5d9d680188472e2a33b7cfca7e5eff40
beto1821 <beto1821@uol.com.br>
2022-08-10 18:46:30 -0300

    desafio18

--- desafio15.sql
+++ desafio15.sql
@@ -1,6 +1,6 @@
 -- Active: 1660079668896@@127.0.0.1@3306@northwind
 SELECT
-    HOUR(submitted_date) AS submitted_hour
+    HOUR(submitted_date) AS 'submitted_hour'
 FROM
     northwind.purchase_orders;
 
--- desafio17.sql
+++ desafio17.sql
@@ -1,3 +1,6 @@
 -- Active: 1660079668896@@127.0.0.1@3306@northwind
-SELECT id, supplier_id FROM northwind.purchase_orders
-WHERE supplier_id = 1 OR supplier_id= 3 OR supplier_id= 5 OR supplier_id= 7 
+SELECT * FROM
+    northwind.purchase_orders
+WHERE
+    supplier_id = 3
+    AND status_id = 2
