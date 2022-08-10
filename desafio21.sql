8686579abcb350dc73048349989b86cb892eced7
beto1821 <beto1821@uol.com.br>
2022-08-10 19:12:33 -0300

    desafio21

---
+++ desafio21.sql
@@ -0,0 +1,38 @@
+-- Active: 1660079668896@@127.0.0.1@3306@northwind
+
+INSERT INTO
+    northwind.order_details (
+        id,
+        order_id,
+        product_id,
+        quantity,
+        unit_price,
+        discount,
+        status_id,
+        date_allocated,
+        purchase_order_id,
+        inventory_id
+    )
+VALUES (
+        NULL,
+        69,
+        80,
+        5.0000,
+        15.0000,
+        0,
+        2,
+        NULL,
+        NULL,
+        129
+    ), (
+        NULL,
+        69,
+        80,
+        5.0000,
+        15.0000,
+        0,
+        2,
+        NULL,
+        NULL,
+        129
+    )
