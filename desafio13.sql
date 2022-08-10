5517e5966a46af827e8ef93ab0814d90488a8746
beto1821 <beto1821@uol.com.br>
2022-08-10 18:01:22 -0300

    desafio14

---
+++ desafio13.sql
@@ -0,0 +1,3 @@
+-- Active: 1660079668896@@127.0.0.1@3306@northwind
+SELECT supplier_id FROM northwind.purchase_orders
+WHERE supplier_id LIKE '1' OR supplier_id LIKE '3'
---
+++ desafio14.sql
@@ -0,0 +1,4 @@
+-- Active: 1660079668896@@127.0.0.1@3306@northwind
+SELECT supplier_id FROM northwind.purchase_orders
+WHERE supplier_id >= 1 AND supplier_id <= 3
+
