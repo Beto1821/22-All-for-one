adc7a1fd54fbdd679856991a80a3712231e169f4
beto1821 <beto1821@uol.com.br>
2022-08-10 18:22:54 -0300

    desafio17

--- desafio15.sql
+++ desafio15.sql
@@ -1,3 +1,6 @@
 -- Active: 1660079668896@@127.0.0.1@3306@northwind
-SELECT HOUR(submitted_date) FROM northwind.purchase_orders
+SELECT
+    HOUR(submitted_date) AS submitted_hour
+FROM
+    northwind.purchase_orders;
 
