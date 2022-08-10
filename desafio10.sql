62db769ab3c8ecb657f80032616c73467120879e
beto1821 <beto1821@uol.com.br>
2022-08-10 17:34:40 -0300

    desafio10 rev1

--- desafio10.sql
+++ desafio10.sql
@@ -1,3 +1,4 @@
 -- Active: 1660079668896@@127.0.0.1@3306@northwind
-SELECT  * FROM northwind.purchase_orders
-ORDER BY created_by >= 3;
+SELECT * FROM northwind.purchase_orders
+WHERE created_by >= 3
+ORDER BY created_by
